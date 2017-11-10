8c5615d9b8383ee87573df3945a2318577bd3406
Sebastian <31518719+sbra4670@users.noreply.github.com>
2017-11-10 14:19:33 -0700

    imported popup display and chatbot packages in ChatbotController

--- src/chat/controller/ChatbotController.java
+++ src/chat/controller/ChatbotController.java
@@ -1,5 +1,8 @@
 package chat.controller;
 
+import  chat.view.PopupDisplay;
+import chat.model.Chatbot;
+
 public class ChatbotController
 {
 	private PopupDisplay display;
@@ -7,14 +10,30 @@ public class ChatbotController
 	
 	public ChatbotController()
 	{
-		
+		chatbot = new Chatbot("Chatbot");
+		display = new PopupDisplay();
 		
 	}
 	
 	
 	public void start()
 	{
+		String response = display.collectResponse("What do you want to talk about?");
 		
+		while (chatbot.lengthChecker(response) && !chatbot.quitChecker(response))
+		{
+			response = popupChat(response);
+			response = display.collectResponse(response);
+		}
+	}
+	
+	private String popupChat(String chat)
+	{
+		String chatbotSays = ""; //Assigns a valid value to a variable that will be returned for the method
+		
+		chatbotSays += chatbot.processConversation(chat);
+		
+		return chatbotSays;
 	}
 	
 }
