3df561eb0edb0cfbbc49bebd648ca77dbb9df3f0
Sebastian <31518719+sbra4670@users.noreply.github.com>
2017-11-15 12:24:21 -0700

    created ChatPanel and commented out errors in project that will soon be fixed.

--- src/chat/view/ChatFrame.java
+++ src/chat/view/ChatFrame.java
@@ -5,5 +5,22 @@ import javax.swing.JFrame;
 
 public class ChatFrame extends JFrame
 {
-
+	private ChatbotController appController;
+	private ChatPanel appPane;	
+	public ChatFrame(ChatbotController appController)
+	{
+		super();
+		this.appController = appController;
+		appPane = new ChatPanel(appController);
+		setupFrame();
+	}
+	
+	private void setupFrame()
+	{
+		this.setTitle("Chatbot.exe");		
+//		this.setContentPane(appPane);
+		this.setSize(600, 600);
+		this.setResizable(false);
+		this.setVisible(true);
+	}
 }
---
+++ src/chat/view/ChatPanel.java
@@ -0,0 +1,12 @@
+package chat.view;
+
+import javax.swing.JPanel;
+import chat.controller.ChatbotController;
+
+public class ChatPanel
+{
+public ChatPanel(ChatbotController appController)
+{
+	super();
+}
+}
